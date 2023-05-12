e745e2b41c195430303f6a641ee056402b3d5c6c
TeuzDio <132937131+TeuzDio@users.noreply.github.com>
2023-05-12 06:14:33 +0200

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -28,7 +28,7 @@ let prova = {
 "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
 }}, "participant": "0@s.whatsapp.net"
 }
-let text = ' 🔱🌀𝕍𝕒𝕝𝕙𝕒𝕝𝕝𝕒-𝔹𝕠𝕥🌀🔱 
+let text = ` 🔱🌀𝕍𝕒𝕝𝕙𝕒𝕝𝕝𝕒-𝔹𝕠𝕥🌀🔱 
 *ciao ${name} in base a quale dispositivo hai, esegui il comando!*
   ඬ ${usedPrefix}MENUANDROID
   ඬ ${usedPrefix}MENUIOS
